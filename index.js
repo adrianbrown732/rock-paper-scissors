1453b5939aacce9c9e550b38831b88c2ddb125d3
Adrian Brown <153460878+adrianbrown732@users.noreply.github.com>
2024-02-20 20:03:40 -0500

    Edit results messages

--- index.js
+++ index.js
@@ -11,25 +11,25 @@ if (userInput === rockPaperSci[compIndex]) { // Tie
 } else if (userInput === "Rock") { // Rock
     switch (compChoice) {
         case "Scissors":
-            console.log(`${compChoice}! YOU WIN!`);
+            console.log(`${userInput} beats ${compChoice}! YOU WIN!`);
             break;
         case "Paper":
-            console.log(`${compChoice}! YOU LOSE!`);
+            console.log(`${compChoice} beats ${userInput}! YOU LOSE!`);
     }
 } else if (userInput === "Paper") { // Paper
     switch (compChoice) {
         case "Rock":
-            console.log(`${compChoice}! YOU WIN!`);
+            console.log(`${userInput} beats ${compChoice}! YOU WIN!`);
             break;
         case "Scissors":
-            console.log(`${compChoice}! YOU LOSE!`);
+            console.log(`${compChoice} beats ${userInput}! YOU LOSE!`);
     }
 } else if (userInput === "Scissors") { // Scissors
     switch (compChoice) {
         case "Paper":
-            console.log(`${compChoice}! YOU WIN!`);
+            console.log(`${userInput} beats ${compChoice}! YOU WIN!`);
             break;
         case "Rock":
-            console.log(`${compChoice}! YOU LOSE!`);
+            console.log(`${compChoice} beats ${userInput}! YOU LOSE!`);
     }
 } 
