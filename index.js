d2b31d7204552d06a98f0770bd0a952ed182273b
Adrian Brown <153460878+adrianbrown732@users.noreply.github.com>
2024-02-20 21:31:00 -0500

    Add getScore()

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let computerChoice = "";
 let computerCompare = "";
 let playerChoice = "";
 let playerCompare = "";
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let compIndex = Math.floor(Math.random() * 3);
@@ -17,32 +19,49 @@ function getPlayerChoice() {
     return playerChoice;
 }
 
+function getScore() {
+    console.log(`PLAYER: ${playerScore} COMPUTER: ${computerScore}`)
+}
+
 function getResult() {
     if (playerCompare === computerCompare) { // Tie
         console.log("It's a TIE!");
+        getScore();
     } else if (playerCompare === "rock") { // Rock
         switch (computerChoice) {
             case "Scissors":
                 console.log(`${playerChoice} beats ${computerChoice}! YOU WIN!`);
+                playerScore++;
+                getScore();
                 break;
             case "Paper":
                 console.log(`${computerChoice} beats ${playerChoice}! YOU LOSE!`);
+                computerScore++;
+                getScore();
         }
     } else if (playerCompare === "paper") { // Paper
         switch (computerChoice) {
             case "Rock":
                 console.log(`${playerChoice} beats ${computerChoice}! YOU WIN!`);
+                playerScore++;
+                getScore();
                 break;
             case "Scissors":
                 console.log(`${computerChoice} beats ${playerChoice}! YOU LOSE!`);
+                computerScore++;
+                getScore();
         }
     } else if (playerCompare === "scissors") { // Scissors
         switch (computerChoice) {
             case "Paper":
                 console.log(`${playerChoice} beats ${computerChoice}! YOU WIN!`);
+                playerScore++;
+                getScore();
                 break;
             case "Rock":
                 console.log(`${computerChoice} beats ${playerChoice}! YOU LOSE!`);
+                computerScore++;
+                getScore();
         }
     }
 }
